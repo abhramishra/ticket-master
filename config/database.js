d391608ec67d6a92729d9488a2f9cb7d9f51e17d
YOUR NAME <abhra.mishra@kloctechnologies.co.in>
2020-04-29 01:25:14 +0530

    fixed bug 1

--- config/database.js
+++ config/database.js
@@ -4,7 +4,8 @@ mongoose.Promise = global.Promise
 // const client = new MongoClient(uri, { useNewUrlParser: true });
 const configureDB = () => {
     const CONNECTION_URI= process.env.MONGODB_URI || "mongodb://localhost:27017/ticket-master-bk"
-    mongoose.connect(CONNECTION_URI,{useNewUrlParser: true})
+    mongoose.connect(CONNECTION_URI,{useNewUrlParser: true, useUnifiedTopology: true})
+    console.log('Mongo URI --------------------', process.env.MONGODB_URI)
     .then(()=>{
         console.log('succesfully connected to db')
     })
