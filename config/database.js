3fcc16d3db12cc91f421e074bf714ce352105e52
YOUR NAME <abhra.mishra@kloctechnologies.co.in>
2020-04-29 11:55:48 +0530

    bug fixed 3

--- config/database.js
+++ config/database.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 const CONNECTION_URI= process.env.MONGODB_URI || "mongodb://localhost:27017/ticket-master-bk"
-mongoose.connect(CONNECTION_URI,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://abhra:secret123@helpdesk-cluster-ovx0p.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true, useUnifiedTopology: true})
 // console.log('Mongo URI --------------------', process.env.MONGODB_URI)
     .then(()=>{
         console.log('succesfully connected to db')
     })
-    .catch(()=>{
-        console.log('error connecting to db')
+    .catch((err)=>{
+        console.log('error connecting to db', err)
     })
+
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
                           
 module.exports = mongoose
