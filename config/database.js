6c782ddff56ed6e494308e82dc50cd4997300d49
YOUR NAME <abhra.mishra@kloctechnologies.co.in>
2020-04-29 11:33:51 +0530

    bug fixed 2

--- config/database.js
+++ config/database.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const configureDB = () => {
-    const CONNECTION_URI= process.env.MONGODB_URI || "mongodb://localhost:27017/ticket-master-bk"
-    mongoose.connect(CONNECTION_URI,{useNewUrlParser: true, useUnifiedTopology: true})
-    console.log('Mongo URI --------------------', process.env.MONGODB_URI)
-        .then(()=>{
-            console.log('succesfully connected to db')
-        })
-        .catch(()=>{
-            console.log('error connecting to db')
-        })
-}
+const CONNECTION_URI= process.env.MONGODB_URI || "mongodb://localhost:27017/ticket-master-bk"
+mongoose.connect(CONNECTION_URI,{useNewUrlParser: true, useUnifiedTopology: true})
+// console.log('Mongo URI --------------------', process.env.MONGODB_URI)
+    .then(()=>{
+        console.log('succesfully connected to db')
+    })
+    .catch(()=>{
+        console.log('error connecting to db')
+    })
                           
-module.exports = configureDB
+module.exports = mongoose
--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 const express = require('express')
-const app = express()
+const mongoose = require('./config/database')
 const routes = require('./config/routes')
 const cors = require('cors')
-require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3000
+//require('dotenv').config()
 const path = require('path')
 
-const port = process.env.PORT || 3000
-const configureDB = require('./config/database')
-configureDB()
 const corsOptions = { exposedHeaders: 'x-auth' }    // to exposed the x-auth token in the response headers
 
 app.use(express.json())
 app.use(cors())
 app.use(cors(corsOptions))
+
 app.use('/', routes)
 
-if (process.env.NODE_ENV == "production") {
-    app.use(express.static(path.join(__dirname,"client/build"))) 
-    app.get("*",(req,res) => { 
-        res.sendFile(path.join(__dirname + "/client/build/index.html")) 
-    }) 
-}
+app.use(express.static(path.join(__dirname,"client/build"))) 
+app.get("*",(req,res) => { 
+    res.sendFile(path.join(__dirname + "/client/build/index.html")) 
+}) 
 
 app.listen(port, () => {
     console.log('Listening to port', port)
